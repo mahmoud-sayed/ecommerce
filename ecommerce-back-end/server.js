7365357c5f604c739148d612493bb83d45e2d9ac
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-08-13 22:19:18 +0200

    done adding all back end but still need to add .env file

--- ecommerce-back-end/server.js
+++ ecommerce-back-end/server.js
@@ -1,16 +1,62 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+const http = require('http');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+require('./connection');
 const server = http.createServer(app);
-const io = new Server(server, { cors: '*', methods: '*' });
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: 'http://localhost:3001',
+  methods: ['GET', 'POST', 'PATCH', "DELETE"]
+});
+
+
+const User = require('./models/User');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const imageRoutes = require('./routes/imageRoutes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+app.use('/images', imageRoutes);
+
+
+app.post('/create-payment', async (req, res) => {
+  const { amount } = req.body;
+  console.log(amount);
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      payment_method_types: ['card']
+    });
+    res.status(200).json(paymentIntent);
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json(e.message);
+  }
+});
+
 
 server.listen(8000, () => {
-  console.log('server is running at port', 8000);
+  console.log('server running at port', 8000);
 });
+const connectionStr = 'mongodb+srv://admin:m-159357258@cluster0.uj2kzj3.mongodb.net/?retryWrites=true&w=majority';
+
+try {
+  mongoose.connect(connectionStr);
+  console.log('connected to the server');
+} catch (error) {
+  console.log(error);
+}
+
+
+app.set('socketio', io);
